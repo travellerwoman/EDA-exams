477bc7b55a3e174c70fc1a1b0d2fe51e69b9967b
travellerwoman <63811457+travellerwoman@users.noreply.github.com>
2022-06-11 16:06:05 +0200

    Done exercise 2 exam 2

--- Examen Final Nº 2/Fuentes/src/examen/sumlevels/SumLevels.java
+++ Examen Final Nº 2/Fuentes/src/examen/sumlevels/SumLevels.java
@@ -1,11 +1,50 @@
 package examen.sumlevels;
 
+import material.Position;
+import material.tree.binarytree.BinaryTree;
+import material.tree.binarytree.LinkedBinaryTree;
+
+import java.util.*;
+
 public class SumLevels {
 
+    BinaryTree<Integer> tree;
 
-	public int sumLevels(int[] levels) {
-        throw new RuntimeException("Not implemented yet");
+
+    public SumLevels() {
+        tree = new LinkedBinaryTree<>();
+    }
+
+    public SumLevels(BinaryTree<Integer> tree) {
+        this.tree = tree;
+    }
+
+    public int sumLevels(Position<Integer> position, Set<Integer> levels, int level) {
+        if (!levels.isEmpty()) {
+            int levelSum = 0;
+            boolean isSameLevel = levels.contains(level);
+            if (isSameLevel) {
+                levelSum += position.getElement();
+            }
+            if (tree.hasLeft(position)){
+                levelSum += sumLevels(tree.left(position), levels, level + 1);
+            } if (tree.hasRight(position)){
+                levelSum += sumLevels(tree.right(position), levels, level + 1);
+            }
+            return levelSum;
+        } return 0;
 	}
+
+    public int sumLevels(int[] levels) {
+        if (levels != null) {
+            Set<Integer> list1 = new HashSet<>();
+            for (int element : levels) {
+                list1.add(element);
+            }
+            return sumLevels(tree.root(), list1, 0);
+        }
+        return 0;
+    }
 
     
 	
